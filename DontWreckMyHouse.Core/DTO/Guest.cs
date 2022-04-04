a750685b8bb421563127c065567a4b34a1a3e6d1
Heng Pho <hengpho98@gmail.com>
2022-04-04 15:44:47 -0500

    Add model for guest

--- DontWreckMyHouse.Core/DTO/Guest.cs
+++ DontWreckMyHouse.Core/DTO/Guest.cs
@@ -8,5 +8,8 @@ namespace DontWreckMyHouse.Core.DTO
 {
     internal class Guest
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
